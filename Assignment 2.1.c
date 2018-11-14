2901309ac55d784e107411992ef824e7551f1e3d
ViVanTam <44991722+ViVanTam@users.noreply.github.com>
2018-11-14 11:06:27 +0700

    Update Assignment 2.1.c

--- Assignment 2.1.c
+++ Assignment 2.1.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+float tim_hinh_chu_nhat(float c, float p)
+{
+	float a,b;
+	p=p/2;
+	(c*c)=(a*a)+ (b*b);
+}
 
+void main()
+{
+	float c,w;
+	printf("Nhap vao canh huyen: ");
+	scanf("%d",&c);
+	printf("Nhap vao chu vi");
+	scanf("%d",&w);
+	tim_hinh_chu_nhat(c,w)
+	getch();
+}
